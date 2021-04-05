885f3c9a8b798bd05ffd2d1ff2a8d6af2acc2b7d
HENGLEASING\Khunlachat.Sa <khunlachat.sa@hengleasing.co.th>
2021-04-05 16:38:26 +0700

    update script

--- Jenkins/sonarqube/jenkins_pipeline.groovy
+++ Jenkins/sonarqube/jenkins_pipeline.groovy
@@ -6,8 +6,8 @@ pipeline{
     environment {
         sonarqube_server_url = "http://localhost:9000"
         sonarqube_token = "your-sonarq-token"
+        sonar_scanner_path = "E:/your/sonar_scanner_path/"
 
-        MSBUILD_PATH = "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin"
         csproj_file = "E:/foo/bar/project1/project1.csproj"
     }
 
@@ -22,7 +22,7 @@ pipeline{
                 bat """
                 cd\\
                 E:
-                cd $NUGET_PATH
+                cd ${NUGET_PATH}
                 nuget.exe restore "E:/foo/bar/project1/package.sln"
                 """
             }
@@ -30,13 +30,13 @@ pipeline{
         stage("MSBuild"){
             steps{
                 bat """
-                set PATH=%PATH%;E:\\Tools\\sonar-scanner-msbuild-5.1.0.28487-net46
+                set PATH=%PATH%;C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin
 
                 C:
-                cd $MSBUILD_PATH
-                SonarScanner.MSBuild.exe begin /k:"your_project_key" /n:"your_project_name" /d:sonar.host.url="$sonarqube_server_url" /d:sonar.login="$sonarqube_token"
-                MSBuild.exe $csproj_file /p:VisualStudioVersion=15.0 /p:Configuration=DEBUG /t:Rebuild
-                SonarScanner.MSBuild.exe end /d:sonar.login="$sonarqube_token"
+                cd ${sonar_scanner_path}
+                SonarScanner.MSBuild.exe begin /k:"your_project_key" /n:"your_project_name" /d:sonar.host.url="${sonarqube_server_url}" /d:sonar.login="${sonarqube_token}"
+                MSBuild.exe ${csproj_file} /p:VisualStudioVersion=15.0 /p:Configuration=DEBUG /t:Rebuild
+                SonarScanner.MSBuild.exe end /d:sonar.login="${sonarqube_token}"
                 """
             }
         }
